fde771e6b14f93825535ee85e2ef57edb71676a6
KhalafAbdi <Abdifatahkhalaf@gmail.com>
2021-04-11 22:06:06 +0200

    Created user graphql and db stuff

---
+++ lib/graphql-resolvers/user.tsx
@@ -0,0 +1,52 @@
+import { findUser, createUser } from '../database/repository/user'
+import Session from '../session'
+
+export default {
+  Query: {
+    async viewer() {},
+    async users() {},
+  },
+  Mutation: {
+    async signUp(_parent: any, _args: any, _context: any, _info: any) {
+      const {
+        name: nameInput,
+        email: emailInput,
+        password: passwordInput,
+      } = _args.input
+
+      //TODO: Validate input
+
+      const user = await createUser(nameInput, emailInput, passwordInput)
+
+      if (!user) {
+        throw new Error('User could not be created, try again later')
+      }
+
+      const { _id, name, email } = user
+      await Session.setLogginSession(_context.res, { _id, name, email })
+
+      console.log(user)
+
+      return user
+    },
+    async signIn(_parent: any, _args: any, _context: any, _info: any) {
+      const { email: emailInput, password: passwordInput } = _args.input
+      //TODO: Validate input
+
+      const user = await findUser(emailInput, passwordInput)
+
+      if (!user) {
+        throw new Error('Invalid email and password combination')
+      }
+
+      const { _id, name, email } = user
+      await Session.setLogginSession(_context.res, { _id, name, email })
+
+      return user
+    },
+    async signOut(_parent: any, _args: any, _context: any, _info: any) {
+      Session.clearLogginSession(_context.res)
+      return true
+    },
+  },
+}
