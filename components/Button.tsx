35d24270f423813211f29b14afb660d08b165d8a
KhalafAbdi <Abdifatahkhalaf@gmail.com>
2021-04-14 19:03:01 +0200

    made button not full width

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,7 +5,7 @@ type ButtonType = {
 
 const Button = ({ text, action }: ButtonType) => {
   return (
-    <div className="flex flex-shrink-0 w-full">
+    <div className="flex flex-shrink-0 w-full sm:w-auto">
       <button
         onClick={() => action()}
         className="bg-gray-800 px-3 py-2 rounded text-gray-50 w-full text-center sm:w-auto focus:outline-none"
