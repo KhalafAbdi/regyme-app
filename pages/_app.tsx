ffa7fd05d94eb3ac765062a40f07e1099e2ae9ce
KhalafAbdi <Abdifatahkhalaf@gmail.com>
2021-04-21 20:37:59 +0200

    Removed apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,10 @@
 import '../styles/global.css'
 import 'tailwindcss/tailwind.css'
 
-import { ApolloProvider } from '@apollo/client'
-import { useApollo } from '../lib/apolo-client'
-
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps.initialApolloState)
-
-  return (
-    <ApolloProvider client={apolloClient}>
-      <div className="container mx-auto">
-        <Component {...pageProps} />
-      </div>
-    </ApolloProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
