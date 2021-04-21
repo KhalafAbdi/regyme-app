2477c03aed4f0a4f2a20a17c74710286f35bc1b4
KhalafAbdi <Abdifatahkhalaf@gmail.com>
2021-04-21 20:40:48 +0200

    created initial exercise page

---
+++ pages/exercises/index.tsx
@@ -0,0 +1,11 @@
+import PrivatePageLayout from '../../components/layouts/PrivatePageLayout'
+
+const Exercises = () => {
+  return (
+    <PrivatePageLayout>
+      <div>Exercises</div>
+    </PrivatePageLayout>
+  )
+}
+
+export default Exercises
