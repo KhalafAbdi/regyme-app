a98838740faf910641c796a7ce7b43630f9b4eb2
KhalafAbdi <Abdifatahkhalaf@gmail.com>
2021-04-21 23:44:00 +0200

    Revert "Merge pull request #4 from KhalafAbdi/Home-page"
    This reverts commit 3fc7206ec77c0d5c131e41a4af872dfccf8266f3, reversing
    changes made to abb989c04f53f8fc090ca672dd8850190ff1e418.

--- components/index.tsx
+++
@@ -1,4 +0,0 @@
-import Logo from './Logo'
-import TextInput from './TextInput'
-
-export { Logo, TextInput }
--- lib/apolo-schema.ts
+++
@@ -1,7 +0,0 @@
-import { makeExecutableSchema } from 'graphql-tools'
-import { typeDefs, resolvers } from './graphql'
-
-export const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-})
