570c2a6ea6a4fb9fc326e728f8c52dbb634906ac
KhalafAbdi <Abdifatahkhalaf@gmail.com>
2021-04-22 18:35:07 +0200

    using button component

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -1,8 +1,10 @@
+import Button from './Button'
+
 type PopupProps = {
   children: JSX.Element
-  onSave: Function
-  onCancel: Function
-  onReset: Function
+  onSave: React.MouseEventHandler<HTMLButtonElement>
+  onCancel: React.MouseEventHandler<HTMLButtonElement>
+  onReset: React.MouseEventHandler<HTMLButtonElement>
   type: String
   text: String
   hasData: boolean
@@ -19,18 +21,13 @@ const Popup = ({
 }: PopupProps) => {
   return (
     <div className="absolute top-0 left-0 bg-gray-800 bg-opacity-25 w-full h-full">
-      <div className="mt-24 bg-gray-100 w-11/12 mx-auto flex flex-col rounded-sm max-w-sm">
-        <div className="flex justify-between ">
+      <div className="mt-24 bg-white w-11/12 mx-auto flex flex-col rounded-sm max-w-sm">
+        <div className="flex justify-between bg-gray-800 bg-opacity-5">
           <div className="px-3 py-3 flex flex-col leading-none">
-            <span className="text-opacity-25 text-lg text-gray-800">
-              {type}
-            </span>
+            <span className="text-lg text-gray-800">{type}</span>
             <span className="font-semibold text-3xl">{text}</span>
           </div>
-          <button
-            className="px-3 py-3 flex items-start"
-            onClick={() => onCancel()}
-          >
+          <button className="px-3 py-3 flex items-start" onClick={onCancel}>
             <svg
               className="w-6 h-6"
               fill="none"
@@ -48,32 +45,25 @@ const Popup = ({
           </button>
         </div>
         <div className="flex-1 bg-white px-3 py-2">{children}</div>
-        <div className="flex justify-between px-3 py-2">
+        <div className="flex justify-between px-3 py-2 bg-gray-800 bg-opacity-5">
           <div>
             {hasData && (
-              <button
-                className="px-5 py-2 rounded-sm text-start"
-                onClick={() => onReset()}
-              >
-                Reset
-              </button>
+              <Button
+                label="Reset"
+                onClick={onReset}
+                type="helper"
+                size="medium"
+              />
             )}
           </div>
           <div className="flex space-x-2">
-            <button
-              className="bg-gray-800 bg-opacity-5 px-5 py-2 rounded-sm underline"
-              onClick={() => onCancel()}
-            >
-              Cancle
-            </button>
-            {onSave && (
-              <button
-                className="bg-gray-800 text-white px-5 py-2 rounded-sm"
-                onClick={() => onSave()}
-              >
-                Save
-              </button>
-            )}
+            <Button
+              label="Cancle"
+              onClick={onCancel}
+              type="secondary"
+              size="medium"
+            />
+            {onSave && <Button label="Save" onClick={onSave} size="medium" />}
           </div>
         </div>
       </div>
