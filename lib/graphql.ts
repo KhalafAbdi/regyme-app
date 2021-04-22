b66463f8b86dc952a8cc9f98842c97506a55be82
KhalafAbdi <Abdifatahkhalaf@gmail.com>
2021-04-22 18:34:48 +0200

    added exercise typedefs and resolvers

--- lib/graphql.ts
+++ lib/graphql.ts
@@ -3,7 +3,10 @@ import merge from 'deepmerge'
 import userTypeDefs from './graphql-typedefs/user'
 import userResolvers from './graphql-resolvers/user'
 
+import exerciseTypeDefs from './graphql-typedefs/exercise'
+import exerciseResolvers from './graphql-resolvers/exercise'
+
 const baseTypeDefs = `type Query type Mutation`
 
-export const typeDefs = [baseTypeDefs, userTypeDefs]
-export const resolvers = merge({}, userResolvers)
+export const typeDefs = [baseTypeDefs, userTypeDefs, exerciseTypeDefs]
+export const resolvers = merge(userResolvers, exerciseResolvers)
