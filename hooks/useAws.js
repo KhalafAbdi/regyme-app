6915c0d9e4c3f89c43b402fde2c51e1383e62cd1
KhalafAbdi <Abdifatahkhalaf@gmail.com>
2021-04-27 20:05:07 +0200

    created aws hooks

---
+++ hooks/useAws.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { poster } from '../util/api'
+
+export const useSignedUrl = () => {
+  return async (payload) => {
+    const mutation = {
+      query:
+        'mutation CreateS3Sign($filename: String!, $filetype: String!) { signS3(filename: $filename, filetype: $filetype) { signeRequest url }}',
+      variables: payload,
+    }
+
+    const { data } = await poster(mutation)
+
+    return data
+  }
+}
+
+export const useUploadtoS3 = async (file, signedRequest) => {
+  const options = {
+    'content-Type': file.type,
+    'x-amz-acl': 'public-read',
+  }
+
+  await axios.put(signedRequest, file, options)
+}
